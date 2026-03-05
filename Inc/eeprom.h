2ca029e1ad87a6142dfb6e9bd202fbd5f6f50190
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-03-05 11:51:47 +0530

    eeprom.h

--- Inc/eeprom.h
+++ Inc/eeprom.h
@@ -38,6 +38,29 @@ uint8_t EEPROM_WriteByte(uint16_t mem_addr, uint8_t data);
 uint8_t EEPROM_ReadByte(uint16_t mem_addr, uint8_t *data);
 
 /*
+ * Function: EEPROM_WritePage
+ * Description:
+ * Writes multiple bytes of data to the EEPROM starting from
+ * the specified memory address. The number of bytes written
+ * must not exceed the EEPROM page size.
+ * Parameters:
+ * address : Starting EEPROM memory address where data
+ *           will be written.
+ 
+ * data    : Pointer to the data buffer that contains
+ *           bytes to be written to EEPROM.
+ * length  : Number of bytes to write. Must be less than
+ *           or equal to EEPROM_PAGE_SIZE.
+ * Returns:
+ * 0  -> Success
+ * 1  -> Error (invalid address or length exceeds page size)
+ * Notes:
+ * This function simulates page write behaviour of EEPROM
+ * devices which allow writing only one page at a time.
+ */
+uint8_t EEPROM_WritePage(uint16_t address,uint8_t *data,uint16_t length);
+
+/*
  * Writes multiple bytes to EEPROM with page-safe handling
  */
 int EEPROM_WriteBuffer(uint16_t mem_addr, uint8_t *data, uint16_t length);
