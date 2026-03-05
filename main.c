4520a412a1f3fd653f57b0ecd980720c3bbb1b63
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-03-05 11:32:52 +0530

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-/*main application file*/
+#include <stdio.h>
+#include "Inc/eeprom.h"
+int main()
+{
+  uint8_t data_read;
+  EEPROM_Init();
+  EEPROM_WriteBytes(10,0x55);
+  EEPROM_ReadByte(10,&data_read);
+  printf("Data read from EEPROM : 0x%X\n", data_read);
+  return 0;
+}
