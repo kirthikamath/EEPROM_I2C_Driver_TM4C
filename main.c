316930c04db7f31f3d3b631e564318ac962aa80b
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-03-06 11:36:24 +0530

    main.c

--- main.c
+++ main.c
@@ -2,10 +2,22 @@
 #include "Inc/eeprom.h"
 int main()
 {
+  uint8_t write_byffer[5]={10,20,30,40,50};
+  uint16_t i;
   uint8_t data_read;
   EEPROM_Init();
+  
+  /*Test single byte write*/
   EEPROM_WriteBytes(10,0x55);
   EEPROM_ReadByte(10,&data_read);
   printf("Data read from EEPROM : 0x%X\n", data_read);
+
+  /*Test page write*/
+  EEPROM_WritePage(20,write_buffer,5);
+  for(i=0;i<5;i++)
+    {
+      EEPROM_ReadByte(20+i,&data_read);
+      printf("Address %d Data %d\n",20+i,data_read);
+    }
   return 0;
 }
