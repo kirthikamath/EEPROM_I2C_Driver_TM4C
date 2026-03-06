4866770b2ad5665febf7ad2e6281b2e1a9119c3a
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-03-06 11:28:00 +0530

    eeprom.c

--- Src/eeprom.c
+++ Src/eeprom.c
@@ -52,17 +52,17 @@ uint8_t EEPROM_ReadByte(uint16_t address, uint8_t *data)
 uint8_t EEPROM_WritePage(uint16_t address,uint8_t *data, uint16_t length)
 {
     uint16_t i;
-    if((address+length )> EEPROM_SIZE_BYTES)
+    if((address+length )> EEPROM_SIZE_BYTES)//prevents writing outside memory.
     {
         return 1;
     }
-    else if (length > EEPROM_PAGE_SIZE)
+    else if (length > EEPROM_PAGE_SIZE)//real eeprom chips allow writing only one page at a time
     {
         return 1;
     }
     else
     {
-        for (i=0; i< length; i++)
+        for (i=0; i< length; i++) //copies each byte from buffer to eeprom memory
         {
             eeprom_memory[address+i]=data[i];
         }
